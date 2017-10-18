a71ccd5a50efa68ea1139df628baff82e2fc23e8
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 13:39:37 -0400

    Create model and controller for Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018152947) do
+ActiveRecord::Schema.define(version: 20171018173418) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "price_in_cents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

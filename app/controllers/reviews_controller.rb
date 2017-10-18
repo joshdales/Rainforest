5692778d670e9dc88f730194c6a0965916ef49f4
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 14:06:13 -0400

    Add method data for create and update reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,25 @@
 class ReviewsController < ApplicationController
 
+  def review_params
+    params.require(:review).permit(:name, :content, :product_id)
+  end
+
   def create
+    @review = Review.new(review_params)
+    @product = Product.find(@review.product_id)
+
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      redirect_to products_path
+    end
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
 
   def update
   end
-  
+
 end

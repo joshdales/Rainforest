ea53b8f1d7c28166219b1ceb77d2f9495a23eeca
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-19 17:16:59 -0400

    Create routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'user/new'
-
-  get 'user/create'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
+
   resources :products do
     resources :reviews
   end
+
+  resource :user, only: [:new, :create]
+
 end

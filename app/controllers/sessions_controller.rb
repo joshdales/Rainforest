5f1024bf21b84ecda2881c0ddf5fc6009ea8d764
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-19 19:13:19 -0400

    Fix typo that was keeping the user form loggin in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:id_id] = user.id
+      session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
       render :new
     end
   end
 
-  def destory
+  def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
   end

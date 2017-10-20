2deac7e87bec27cd7c4c814d2ea1b7ad99fc3c69
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-20 15:40:05 -0400

    Create user_id column in Reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019205427) do
+ActiveRecord::Schema.define(version: 20171020193709) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171019205427) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

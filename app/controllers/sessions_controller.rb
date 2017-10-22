7075347b02cf7ba0e71c512a647e6fea37a260f0
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-22 09:08:49 -0400

    Add responsive element to products

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      render :new, alert: @user.errors.full_messages
+      render :new, alert: "Your Username or Password is incorrect"
     end
   end
 
